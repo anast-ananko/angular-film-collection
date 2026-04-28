9d2bc9d485bf6d586cdbc617f49eaf25e3c2ed7a
Nastassia Ananka <anast.ananko@gmail.com>
2026-04-28 11:05:19 +0200

    feat: add routes and basic components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { FilmDetailsComponent } from './pages/film-details/film-details.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: ':id', component: FilmDetailsComponent },
+  { path: '**', component: NotFoundComponent },
+];
