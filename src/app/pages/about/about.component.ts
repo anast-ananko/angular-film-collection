9ce1de01478579a4e1e57314b0645f1af1291285
Nastassia Ananka <anast.ananko@gmail.com>
2026-04-29 16:08:59 +0200

    feat: add about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
